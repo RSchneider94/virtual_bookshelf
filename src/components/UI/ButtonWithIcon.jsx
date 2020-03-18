eeb235852fec42e3f4e2f7b411ab9cacdd5dc498
RSchneider94 <rodrigoschneider94@gmail.com>
2020-03-18 11:19:52 +0100

    Make custom button to accept styles

--- src/components/UI/ButtonWithIcon.jsx
+++ src/components/UI/ButtonWithIcon.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
-  returnButton: {
-    marginTop: 50
-  }
-});
-
-export default function ButtonWithIcon({ icon, text, clickHandler }) {
-  const classes = useStyles();
-
+export default function ButtonWithIcon({
+  icon,
+  text,
+  clickHandler,
+  customStyles
+}) {
   return (
     <Button
       variant="contained"
-      className={classes.returnButton}
+      style={customStyles ? customStyles : null}
       startIcon={icon}
       onClick={clickHandler}
     >
